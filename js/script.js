4df9b36b61cb25c9b9359bcbabba07515b56482a
Shahana Akter <64885153+shahana-12@users.noreply.github.com>
2021-06-06 22:49:24 +0600

    add all files

---
+++ js/script.js
@@ -0,0 +1,193 @@
+
+$('#banner-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    fade: true,
+    fadeDuration: 2000,
+    autoplaySpeed: 2000,
+  });
+
+  $('.screenshot_slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  $('.venobox').venobox(); 
+
+  $('.team_members').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow:3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  $('.slider_details').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow:1,
+    autoplay: true,  
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: ".img_slider",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.img_slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow:3,
+    autoplay: true,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "1",
+    asNavFor: ".slider_details",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+  $(document).ready(function() {
+    // grab the initial top offset of the navigation 
+       var stickyNavTop = $('.navbar').offset().top;
+       
+       // our function that decides weather the navigation bar should have "fixed" css position or not.
+       var stickyNav = function(){
+        var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+             
+        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+        // otherwise change it back to relative
+        if (scrollTop > stickyNavTop) { 
+            $('.navbar').addClass('sticky');
+        } else {
+            $('.navbar').removeClass('sticky'); 
+        }
+    };
+  
+    stickyNav();
+    // and run it again every time you scroll
+    $(window).scroll(function() {
+      stickyNav();
+    });
+  });
+
+
+
